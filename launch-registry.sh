44381ca9de4a444fb9ee53b3a8ebdcc92ad7a113
Sam Vevang <sam.vevang@gmail.com>
2024-11-13 03:44:25 +0000

    Use ssl

--- launch-registry.sh
+++ launch-registry.sh
@@ -5,4 +5,7 @@ docker run -d \
   -p 192.168.1.108:5000:5000 \
   -v $(pwd)/registry-data:/var/lib/registry \
   -v $(pwd)/config.yml:/etc/distribution/config.yml \
+  -v $(pwd)/certs:/certs:ro \
+  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+  -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
   registry:2
